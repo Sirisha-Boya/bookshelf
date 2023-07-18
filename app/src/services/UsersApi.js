ad4937513da5a157ac5245c93f49b238fcc1a85e
sirisha-xyram <sirishab431@gmail.com>
2023-07-18 12:29:00 +0530

    url changed

--- app/src/services/UsersApi.js
+++ app/src/services/UsersApi.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
+const BaseUrl = "http://16.171.34.31:80/api"
 export const Register = async (obj) => {
   var res = await axios
-    .post("http://localhost:3002/api/register", obj)
+    .post(`${BaseUrl}/register`, obj)
     .then((response) => {
       //console.log("stark",response)
       return response.data;
@@ -16,7 +17,7 @@ export const Register = async (obj) => {
 
 export const Login = async (obj) => {
   var res = await axios
-    .post("http://localhost:3002/api/login", obj)
+    .post(`${BaseUrl}/login`, obj)
     .then((response) => {
       //console.log("stark",response)
   
@@ -31,7 +32,7 @@ export const Login = async (obj) => {
 
 export const Users = async () => {
   var res = await axios
-    .get("http://localhost:3002/api/users")
+    .get(`${BaseUrl}/users`)
     .then((response) => {
       return response.data;
     })
