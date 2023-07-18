2566b74634685a647e114ef57378e145c5338f98
sirisha-xyram <sirishab431@gmail.com>
2023-07-18 11:32:06 +0530

    Build code added in backend server

--- server/index.js
+++ server/index.js
@@ -3,9 +3,8 @@ const cors = require("cors");
 const connectDB = require("./dbConfig");
 const userRoutes = require("./routes/userRoutes");
 const bookRoutes = require("./routes/bookRoutes");
-
+const path = require("path");
 const PORT = 3002;
-// const ipAddress = "10.31.21.201"
 connectDB();
 
 const app = express();
@@ -18,7 +17,20 @@ app.use(cors());
 app.use("/api", userRoutes);
 app.use("/api", bookRoutes);
 
+const buildPath = path.join(__dirname, "../app/build");
+app.use(express.static(buildPath));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "../app/build/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
 // Start the server
+// app.listen(process.env.PORT, process.env.IP_ADDRESS, () => {
+//   console.log(`Server is running on port ${process.env.PORT}`);
+// });
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
